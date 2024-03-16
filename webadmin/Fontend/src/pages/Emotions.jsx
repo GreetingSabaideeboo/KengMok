51373504570e86a77dc8ee9c3f7ab1c2e47bab33
TJ PACHARA <pacharatemjai@gmail.com>
2024-03-17 01:51:41 +0700

    fix top box emotion page

--- webadmin/Fontend/src/pages/Emotions.jsx
+++ webadmin/Fontend/src/pages/Emotions.jsx
@@ -100,9 +100,11 @@ function Emotions () {
         <>
         <div className="container-emotion">
             <div className="second-box">
-                <div className="top-emotion">Emotions Manager</div>
+                <div className="top-emotion">Emotions</div>
+                <div className="top-emotion">&nbsp;</div>
+                <div className="top-emotion">Manager</div>
                 <div className="space"></div>
-                <Link to={"/addsound"} className="butaddsound">Add</Link>
+                <Link to={"/addsound"} className="butaddsound">Add text response</Link>
                 {/* <div className="space"> </div> */}
                 {/* <button onClick={emotionlist} className="butlistemo">List</button> */}
             </div> 
